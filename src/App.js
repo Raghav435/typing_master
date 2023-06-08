e9773c6016a356a0b58c734da5b7b6cf27b43b65
Raghav Shukla <shuklaraghav435@gmail.com>
2023-06-08 13:30:44 +0530

    updated the app.js with multiple operation & update word.js with some changes

--- src/App.js
+++ src/App.js
@@ -1,176 +1,187 @@
-import { useState, useEffect, useRef } from "react";
-import randomWords from "random-words";
-const NUMB_OF_WORDS = 200;
-const SECONDS = 60;
+import { useEffect, useRef, useState } from "react";
+import { words } from "./Utils/words";
+import "./App.css";
+var current = 0;
+var correct = new Array(100).fill(0);
+const Word = ({ word, index }) => {
+  var color;
+  if (current === index) {
+    color = "#f1fa8c";
+  } else if (correct[index] === 1) {
+    color = "#50fa7b";
+  } else if (correct[index] === -1) {
+    color = "#ff5555";
+  }
+
+  return <span style={{ color: color }}>{word} </span>;
+};
+var timeRunning = false,
+  time = 0,
+  timer,
+  correctNumber = 0;
 
 function App() {
-  const [words, setWords] = useState([]);
-  const [countDown, setCountDown] = useState(SECONDS);
-  const [currInput, setCurrInput] = useState("");
-  const [currWordIndex, setCurrWordIndex] = useState(0);
-  const [currCharIndex, setCurrCharIndex] = useState(-1);
-  const [currChar, setCurrChar] = useState("");
-  const [correct, setCorrect] = useState(0);
-  const [incorrect, setIncorrect] = useState(0);
-  const [status, setStatus] = useState("waiting");
-  const textInput = useRef(null);
+  const [inputValue, setInputValue] = useState("");
+  const [sentenceArr, setSentenceArr] = useState([]);
+  const [speed, setSpeed] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
+  const [checked, setChecked] = useState(15);
+  const [highest, setHighest] = useState(0);
+  const [last, setLast] = useState(0);
+  const inputRef = useRef(null);
 
+  const getRandomWords = (n) => {
+    if (!n) n = checked;
+    let random = words.sort(() => 0.5 - Math.random()).slice(0, n);
+    setSentenceArr(random);
+    inputRef.current.focus();
+  };
   useEffect(() => {
-    setWords(generateWords());
+    const localhigest = localStorage.getItem("highest");
+    if (localhigest) setHighest(localhigest);
+    const localChecked = localStorage.getItem("checked");
+    if (localChecked) setChecked(Number(localChecked));
+    getRandomWords(localChecked);
+    //eslint-disable-next-line
   }, []);
-
-  useEffect(() => {
-    if (status === "started") {
-      textInput.current.focus();
+  const handleInput = (e) => {
+    const tempInput = e.target.value;
+    if (tempInput[tempInput.length - 1] === " ") return;
+    setInputValue(tempInput);
+    if (current === checked - 1 && tempInput === sentenceArr[checked - 1]) {
+      handleSubmit(tempInput);
+      setLast(speed);
+      localStorage.setItem("highest", Math.max(highest, speed));
+      if (highest < speed) setHighest(speed);
+      else setHighest(Number(localStorage.getItem("highest")));
+      timerClearer();
+      setInputValue("");
+      return;
     }
-  }, [status]);
+  };
+  const handleSubmit = (tempInput) => {
+    if (
+      inputValue === sentenceArr[current] ||
+      tempInput === sentenceArr[current]
+    ) {
+      correct[current] = 1;
+      correctNumber++;
+    } else correct[current] = -1;
 
-  function generateWords() {
-    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
-  }
+    setAccuracy(((correctNumber / (current + 1)) * 100).toFixed(0));
+    current++;
 
-  function start() {
-    if (status === "finished") {
-      setWords(generateWords());
-      setCurrWordIndex(0);
-      setCorrect(0);
-      setIncorrect(0);
-      setCurrCharIndex(-1);
-      setCurrChar("");
+    if (current === sentenceArr.length) {
+      // timerClearer();
+      restart(checked);
     }
-
-    if (status !== "started") {
-      setStatus("started");
-      let interval = setInterval(() => {
-        setCountDown((prevCountdown) => {
-          if (prevCountdown === 0) {
-            clearInterval(interval);
-            setStatus("finished");
-            setCurrInput("");
-            return SECONDS;
-          } else {
-            return prevCountdown - 1;
-          }
-        });
+  };
+  const handleKeyPress = (e) => {
+    if (e.key !== " " || current === checked || e.target.value.trim() === "")
+      return false;
+    if (!timeRunning && current < sentenceArr.length) {
+      timeRunning = true;
+      timer = setInterval(() => {
+        time++;
+        setSpeed(((60 * correctNumber) / time).toFixed(0));
       }, 1000);
     }
-  }
-
-  function handleKeyDown({ keyCode, key }) {
-    // space bar
-    if (keyCode === 32) {
-      checkMatch();
-      setCurrInput("");
-      setCurrWordIndex(currWordIndex + 1);
-      setCurrCharIndex(-1);
-      // backspace
-    } else if (keyCode === 8) {
-      setCurrCharIndex(currCharIndex - 1);
-      setCurrChar("");
-    } else {
-      setCurrCharIndex(currCharIndex + 1);
-      setCurrChar(key);
-    }
-  }
-
-  function checkMatch() {
-    const wordToCompare = words[currWordIndex];
-    const doesItMatch = wordToCompare === currInput.trim();
-    if (doesItMatch) {
-      setCorrect(correct + 1);
-    } else {
-      setIncorrect(incorrect + 1);
-    }
-  }
-
-  function getCharClass(wordIdx, charIdx, char) {
-    if (
-      wordIdx === currWordIndex &&
-      charIdx === currCharIndex &&
-      currChar &&
-      status !== "finished"
-    ) {
-      if (char === currChar) {
-        return "has-background-success";
-      } else {
-        return "has-background-danger";
-      }
-    } else if (
-      wordIdx === currWordIndex &&
-      currCharIndex >= words[currWordIndex].length
-    ) {
-      return "has-background-danger";
-    } else {
-      return "";
-    }
-  }
-
+    handleSubmit();
+    setInputValue("");
+  };
+  const timerClearer = () => {
+    clearInterval(timer);
+    timeRunning = false;
+    time = 0;
+    correctNumber = 0;
+  };
+  const restart = (number) => {
+    correct.fill(0);
+    current = 0;
+    getRandomWords(number);
+    setSpeed(0);
+    setAccuracy(0);
+    timerClearer();
+  };
+  const handleRadio = (e) => {
+    const value = e.target.value;
+    restart(value);
+    localStorage.setItem("checked", Number(value));
+    setChecked(Number(value));
+  };
   return (
-    <div className="App">
-      <div className="section">
-        <div className="is-size-1 has-text-centered has-text-primary">
-          <h2>{countDown}</h2>
+    <>
+      <div className="home">
+        <form className="radio-container">
+          Select No. of Words:
+          <div className="radio">
+            <input
+              type="radio"
+              name="number"
+              id="radio-1"
+              value={15}
+              onChange={handleRadio}
+              checked={checked === 15}
+            />
+            <span>15</span>
+          </div>
+          <div className="radio">
+            <input
+              type="radio"
+              name="number"
+              id="radio-2"
+              value={30}
+              onChange={handleRadio}
+              checked={checked === 30}
+            />
+            <span>30</span>
+          </div>
+          <div className="radio">
+            <input
+              type="radio"
+              name="number"
+              id="radio-3"
+              value={45}
+              onChange={handleRadio}
+              checked={checked === 45}
+            />
+            <span>45</span>
+          </div>
+          <div className="radio">
+            <input
+              type="radio"
+              name="number"
+              id="radio-4"
+              value={60}
+              onChange={handleRadio}
+              checked={checked === 60}
+            />
+            <span>60</span>
+          </div>
+        </form>
+        <div className="sentence-container">
+          <span className="speed">
+            {" "}
+            Current speed (WPM): {speed}
+            <span className="highest"> Last Round: {last}</span>
+            <span className="highest"> Highest: {highest}</span>
+          </span>
+          <span className="speed accuracy"> Accuracy: {accuracy}%</span>
+          {sentenceArr.map((word, index) => {
+            return <Word key={index} word={word} index={index} />;
+          })}
         </div>
-      </div>
-      <div className="control is-expanded section">
         <input
-          ref={textInput}
-          disabled={status !== "started"}
+          ref={inputRef}
           type="text"
-          className="input"
-          onKeyDown={handleKeyDown}
-          value={currInput}
-          onChange={(e) => setCurrInput(e.target.value)}
+          className="type-input"
+          placeholder="Start Typing"
+          value={inputValue}
+          onChange={handleInput}
+          onKeyPress={handleKeyPress}
         />
       </div>
-      <div className="section">
-        <button className="button is-info is-fullwidth" onClick={start}>
-          Start
-        </button>
-      </div>
-      {status === "started" && (
-        <div className="section">
-          <div className="card">
-            <div className="card-content">
-              <div className="content">
-                {words.map((word, i) => (
-                  <span key={i}>
-                    <span>
-                      {word.split("").map((char, idx) => (
-                        <span className={getCharClass(i, idx, char)} key={idx}>
-                          {char}
-                        </span>
-                      ))}
-                    </span>
-                    <span> </span>
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {status === "finished" && (
-        <div className="section">
-          <div className="columns">
-            <div className="column has-text-centered">
-              <p className="is-size-5">Words per minute:</p>
-              <p className="has-text-primary is-size-1">{correct}</p>
-            </div>
-            <div className="column has-text-centered">
-              <p className="is-size-5">Accuracy:</p>
-              {correct !== 0 ? (
-                <p className="has-text-info is-size-1">
-                  {Math.round((correct / (correct + incorrect)) * 100)}%
-                </p>
-              ) : (
-                <p className="has-text-info is-size-1">0%</p>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
 
